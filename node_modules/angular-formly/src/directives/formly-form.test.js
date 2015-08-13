bb78caa57ac4c68b2cdb3699dc309901daa53de9
Kent C. Dodds <kent@doddsfamily.us>
2015-08-12 21:49:36 -0500

    Removing node_modules

--- node_modules/angular-formly/src/directives/formly-form.test.js
+++
@@ -1,8 +0,0 @@
-module.exports = ngModule => {
-  describe('formly-form', () => {
-    beforeEach(window.module(ngModule.name));
-    it('should be tested in the future', () => {
-      expect(true).to.be.true;
-    });
-  });
-};
--- node_modules/angular-formly/src/index.test.js
+++
@@ -1,8 +0,0 @@
-require('./angular-fix');
-require('angular-mocks/angular-mocks');
-window.apiCheck = require('api-check');
-const chai = require('chai');
-const sinonChai = require('sinon-chai');
-window.expect = chai.expect;
-chai.use(sinonChai);
-module.exports = require('./index.common');
